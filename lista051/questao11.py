bf664f237fb5e01b2354c3febcca233cc2f9af47
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-21 11:41:03 -0300

    a

--- lista051/questao11.py
+++ lista051/questao11.py
@@ -5,4 +5,13 @@ expoente qualquer (Variável e), ou seja, de be. (Sem usar Math.pow();)
 base = int(input("Informe uma base: "))
 exp = int(input("Informe um expoente: "))
 
+cont = 1
+potencia = base
+
 print(f"O resultado da potência é {base ** exp}")
+
+while (cont < exp):
+    potencia = potencia * base
+    cont += 1
+
+print(f"O resultado da potência é {potencia}")
