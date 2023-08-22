d7761a6c9b81a8ffaab22bcdcfa33c82cc10460e
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-22 11:17:29 -0300

    a

--- lista051/questao12.py
+++ lista051/questao12.py
@@ -4,9 +4,8 @@ menor número que foram digitados, além da media entre TODOS os números digita
 de números deve parar quando o usuário digitar o número -1, e este número -1 não deve ser considerado nem
 como maior, nem como menor, e nem na contagem da media
 """
-import math
 
-num = int(input("Informe um número (-1 para realizar a operação): "))
+num = float(input("Informe um número (-1 para realizar a operação): "))
 
 maior = num
 menor = num
@@ -14,12 +13,11 @@ media = num
 i = 1
 
 while num != -1:
-    num = int(input("Informe um número (-1 para realizar a operação): "))
+    num = float(input("Informe um número (-1 para realizar a operação): "))
 
     if num != -1:
         media += num
         i += 1
-
         if maior < num:
             maior = num
 
